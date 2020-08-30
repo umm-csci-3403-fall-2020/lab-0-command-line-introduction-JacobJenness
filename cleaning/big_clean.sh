7e5cd12b73db770e2d7ca90f90300e299a17ca9a
JacobJenness <jenne077@morris.umn.edu>
2020-08-30 16:32:16 -0500

    Update archive creation
    This commit has updated the creation process of the new "cleaned" tar file.

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
+# Creates the "tar_file" from the command line
+# Creates the temporary directory "SCRATCH"
+# Creates the base name "base"
+# Creates the working directory "here"
 tar_file=$1
 SCRATCH=$(mktemp -d)
-
-tar -zxf "$tar_file" --directory="$SCRATCH"
-
-find "$SCRATCH" -type f -name "DELETE ME!" -delete
-
-# Create a new tar file with the cleaned files
+base=$(basename "$SCRATCH/$tar_file" .tgz)
 here=$(pwd)
-cd $SCRATCH
-tar -zcf cleaned_$tar_file.tgz "$SCRATCH"
+# echo $tar_file $SCRATCH $base $here
+
+# Extracts tar_file into SCRATCH
+tar -zxf "$tar_file" -C "$SCRATCH"
+# ls $SCRATCH/$base
+
+# Finds all instances of "DELETE ME!" in SCRATCH and deletes them
+grep -Rl "DELETE ME!" "$SCRATCH" | xargs rm
+# ls $SCRATCH/$base
+
+# Creates a new tar file with the cleaned files
+cd "$SCRATCH"
+tar -zcf cleaned_"$tar_file" "$SCRATCH/$base"
+ls $SCRATCH/$base
+
+# Move the new tar file into the pwd
+mv cleaned_"$tar_file" $here
+cd "$here"
+
